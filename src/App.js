ba3de98888850eb1f0cd1464035abc32f626bfd1
Lamia Moshrif <lamiamoshrif@gmail.com>
2021-01-03 09:58:05 +0100

    Importing the header in app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     // BEM 
     <div className="app">
       <h1>Hello Clever programmmers </h1>
+      <Header />
+      {/* Home */}
     </div>
   );
 }
