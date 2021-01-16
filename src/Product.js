be341ad8f3e5272dffce827e6af8e77f93ba5774
Lamia Moshrif <lamiamoshrif@gmail.com>
2021-01-16 12:56:02 +0100

    Products added in Home.js

--- src/Product.js
+++ src/Product.js
@@ -1,21 +1,23 @@
 import React from 'react';
 import "./Product.css";
 
-function Product() {
+function Product({title, image, price, rating }) {
     return (
         <div className="product">
             <div className="product__info">
-                <p>The lean Startup</p>
+                <p>{title}</p>
                 <p className="product__price">
                     <small>$</small>
-                    <strong>19.99</strong>
+                    <strong>{price}</strong>
                 </p>
                 <div className="product__rating">
-                    <p>⭐</p>
+                    {Array(rating).fill().map((_, i) => (
+                        <p>⭐</p>
+                    ))}   
                 </div>
             </div>
 
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRLDGCIreAxtmYES-b0drzc9kxKX5KwHC37Y7tNeuJIxQXCl6TOkQMiDPzxgwnq2IpBrpKtQ8ED&usqp=CAc" alt="" />
+            <img src={image} alt="" />
             <button>Add to Basket</button>
         </div>
     )
