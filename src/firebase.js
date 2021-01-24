96cff51fb94ef99e649b20129e99de3f1326731b
Lamia Moshrif <lamiamoshrif@gmail.com>
2021-01-24 18:55:16 +0100

    initializing the app and the database

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from "firebase";
+
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
     apiKey: "AIzaSyBQjr8_f5busApkF6weC6-OdLwMgOERj2M",
@@ -8,3 +10,10 @@ const firebaseConfig = {
     appId: "1:50592112785:web:7b58bb7aff42f80f6a1f30",
     measurementId: "G-HEVQ3T235Y"
   };
+
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+  const db = firebaseApp.firestore();
+  const auth = firebase.auth();
+
+  export { db, auth };
