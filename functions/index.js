53a704635955f75291d4bc54973081160b115b25
Lamia Moshrif <lamiamoshrif@gmail.com>
2021-01-31 17:23:31 +0100

    Setting up the Api - full backend

--- functions/index.js
+++ functions/index.js
@@ -14,7 +14,26 @@ app.use(cors ({ origin: true}));
 app.use(express.json());
 
 // API routes
+// APIs have different types of request, get requests and post requests
+
 app.get('/', (request, response) => response.status(200).send('hello world'))
+
+app.post ('/payments/create', async (request, response) => {
+    const total = request.query.total;
+
+    console.log('Payment Request Recieved BOOM ! ! ! for this amount >>>>>' , total)
+
+    const paymentIntent = await stripe.paymentIntents.create({
+            amount: total, // subunits of the currency
+            currency: "usd",
+    });
+
+    // OK - Created 
+    response.status(201).send({
+        clientSecret: paymentIntent.client_secret,
+    })
+
+})
 
 // Listen command
 
